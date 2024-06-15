67a54e97b4c228e83fabca7d0d37ba0d6c5e4d0a
Samuel Omanchi <omanchis@gmail.com>
2024-06-15 15:33:57 +0200

    Initial Commit

---
+++ MacEnhance/MacEnhanceApp.swift
@@ -0,0 +1,20 @@
+//
+//  MacEnhanceApp.swift
+//  MacEnhance
+//
+//  Created by Samuel Enyi Omanchi on 15.06.24.
+//
+
+import SwiftUI
+
+@main
+struct MacEnhanceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
