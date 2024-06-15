4ded5229c9adc5c5bb43cb188b3c17a12f818f87
Samuel Omanchi <omanchis@gmail.com>
2024-06-15 21:51:56 +0200

    add deleting clipboard items

---
+++ AnotherToolView.swift
@@ -0,0 +1,8 @@
+//
+//  AnotherToolView.swift
+//  MacEnhance
+//
+//  Created by Samuel Enyi Omanchi on 15.06.24.
+//
+
+import Foundation
---
+++ ToolSelectionView.swift
@@ -0,0 +1,8 @@
+//
+//  ToolSelectionView.swift
+//  MacEnhance
+//
+//  Created by Samuel Enyi Omanchi on 15.06.24.
+//
+
+import Foundation
